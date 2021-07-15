289d45a9380277c59c7b1d25e5a8219a7311fe0a
Raghav Kasyap <raghavk2503@gmail.com>
2021-07-15 20:15:23 +0530

    Added the user actions creater and user reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,18 +4,28 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer } from './reducers/productReducers'
 import { productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 })
 
 const cartItemFromStorage = localStorage.getItem('cartItems')
     ? JSON.parse(localStorage.getItem('cartItems'))
     : []
 
-const initialState = { cart: { cartItems: cartItemFromStorage } }
+const userInfoFromStorage = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null
+
+const initialState = {
+    cart: { cartItems: cartItemFromStorage },
+    userLogin: { userInfor: userInfoFromStorage },
+}
+
 const middleware = [thunk]
 
 const store = createStore(
